d3933ccf3bd39dc234ffb4d58198df39d19f715d
Mitchbux <55109545+Mitchbux@users.noreply.github.com>
2020-05-08 01:48:27 +0200

    Official Comments

--- two.js
+++ two.js
@@ -1,11 +1,19 @@
 ("script")
 
-js [hello] {say:{hello:"world"}},
+# //Hello world in an Indexer Filter
+# //the js keyword suggest to treat the code as JSON data
 
+js [hello] {say: {hello:"world"} },
+
+# //This stack only logs Hello JSON data onto the console
+# //There is only one item in Hello JSON data : say
 {
-	for(var v in js.hello)
-		console.log(v + ' => ' + JSON.stringify(js.hello[v]));
-	return this.join("") + " end.";
+	for(var o in js.hello)
+		console.log(o + ' => ' +      // o is the only object [say]
+		JSON.stringify(js.hello[o])); // js.hello[say] is an object, use stringify to display it
+	
+	//# this stack creates a 'script end.' message information
+	return this.join('') + " end.";
 }
 
 
