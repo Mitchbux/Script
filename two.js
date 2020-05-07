68c676042530868c8c1d07fb674d43e7032e9645
Mitchbux <55109545+Mitchbux@users.noreply.github.com>
2020-05-08 01:50:10 +0200

    Indentation

--- two.js
+++ two.js
@@ -8,9 +8,9 @@ js [hello] {say: {hello:"world"} },
 # //This stack only logs Hello JSON data onto the console
 # //There is only one item in Hello JSON data : say
 {
-	for(var o in js.hello)
-		console.log(o + ' => ' +      // o is the only object [say]
-		JSON.stringify(js.hello[o])); // js.hello[say] is an object, use stringify to display it
+	for(var say in js.hello)
+		console.log(say + ' => ' +      // ['say'] is the only object
+		JSON.stringify(js.hello[say])); // js.hello[say] is an object, use stringify to display it
 	
 	//# this stack creates a 'script end.' message information
 	return this.join('') + " end.";
